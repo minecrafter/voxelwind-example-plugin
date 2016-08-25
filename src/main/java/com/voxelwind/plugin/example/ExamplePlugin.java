26dc25b24a88f0b2a7937d79b659aee872fc8130
TheDiamondYT1 <goldowl37@gmail.com>
2016-08-25 20:16:50 +0100

    Add ServerStartEvent and README

--- src/main/java/com/voxelwind/plugin/example/ExamplePlugin.java
+++ src/main/java/com/voxelwind/plugin/example/ExamplePlugin.java
@@ -1,13 +1,22 @@
 package com.voxelwind.plugin.example;
 
 import com.voxelwind.api.plugin.Plugin;
+import com.voxelwind.api.server.Player;
 import com.voxelwind.api.server.event.Listener;
 import com.voxelwind.api.server.event.server.ServerStartEvent;
+import com.voxelwind.api.server.event.player.PlayerSpawnEvent;
 
 @Plugin(id = "example-plugin", author = "tuxed", version = "0.1")
 public class ExamplePlugin {
     @Listener
     public void onServerStart(ServerStartEvent event) {
         System.out.println("Hello world!");
+    }
+
+    @Listener
+    public void onPlayerSpawn(PlayerSpawnEvent event) {
+        Player player = event.getPlayer();
+        System.out.println(player.getName() + " has spawned in level " + player.getLevel().getName() +
+                " in gamemode " + player.getGameMode().name().toLowerCase());
     }
 }
