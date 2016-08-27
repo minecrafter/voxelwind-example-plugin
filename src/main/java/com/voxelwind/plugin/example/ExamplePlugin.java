eecf3d4766c629cf118571919ef1db2db82e4015
Tux <write@imaginarycode.com>
2016-08-27 00:39:27 -0400

    Added dependency injection (Logger and PluginDescription)

--- src/main/java/com/voxelwind/plugin/example/ExamplePlugin.java
+++ src/main/java/com/voxelwind/plugin/example/ExamplePlugin.java
@@ -1,13 +1,25 @@
 package com.voxelwind.plugin.example;
 
+import com.google.inject.Inject;
 import com.voxelwind.api.plugin.Plugin;
+import com.voxelwind.api.plugin.PluginDescription;
 import com.voxelwind.api.server.event.Listener;
 import com.voxelwind.api.server.event.server.ServerStartEvent;
+import org.slf4j.Logger;
 
 @Plugin(id = "example-plugin", author = "tuxed", version = "0.1")
 public class ExamplePlugin {
+    private final Logger logger;
+    private final PluginDescription description;
+
+    @Inject
+    public ExamplePlugin(Logger logger, PluginDescription description) {
+        this.logger = logger;
+        this.description = description;
+    }
+
     @Listener
     public void onServerStart(ServerStartEvent event) {
-        System.out.println("Hello world!");
+        logger.info("Hello world from " + description.getId());
     }
 }
