a31ed81a52586d5a50c263347d4c2cd31be1c983
Tux <write@imaginarycode.com>
2016-08-28 01:43:47 -0400

    Update for injection changes

--- src/main/java/com/voxelwind/plugin/example/ExamplePlugin.java
+++ src/main/java/com/voxelwind/plugin/example/ExamplePlugin.java
@@ -1,11 +1,12 @@
 package com.voxelwind.plugin.example;
 
-import com.google.inject.Inject;
 import com.voxelwind.api.plugin.Plugin;
 import com.voxelwind.api.plugin.PluginDescription;
 import com.voxelwind.api.server.event.Listener;
 import com.voxelwind.api.server.event.server.ServerStartEvent;
 import org.slf4j.Logger;
+
+import javax.inject.Inject;
 
 @Plugin(id = "example-plugin", author = "tuxed", version = "0.1")
 public class ExamplePlugin {
